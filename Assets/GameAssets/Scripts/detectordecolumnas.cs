8421664120346680183c8f12320cfc526a49f8db
zeuxander <161500324+zeuxander@users.noreply.github.com>
2024-02-29 20:47:50 +0100

    mas codigo

--- Assets/GameAssets/Scripts/detectordecolumnas.cs
+++ Assets/GameAssets/Scripts/detectordecolumnas.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class detectordecolumnas : MonoBehaviour
 {
+    public TMP_Text textObject;
   public int puntuacion = 0;
 
     public int puntosPorColumna = 1;
-
-    private void OnTriggerExit(Collider other)
+    void OnTriggerExit()
     {
         puntuacion = puntuacion + puntosPorColumna;
+        textObject.GetComponentInChildren<TextMeshProUGUI>().SetText(puntuacion.ToString());
     }
+    
+        
+    
 
-   
 }
